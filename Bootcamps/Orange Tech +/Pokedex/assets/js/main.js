4b86ba2cf4416c835cb3c0698f621477aefc07a9
levifmorais <102878183+levifmorais@users.noreply.github.com>
2022-11-06 17:35:00 -0300

    tipos e numeração

--- Bootcamps/Orange Tech +/Pokedex/assets/js/main.js
+++ Bootcamps/Orange Tech +/Pokedex/assets/js/main.js
@@ -1,1 +1,30 @@
 
+
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+                <span class="number">${'#'+pokemon.id.toString().padStart(3,'0')}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                    </ol>
+                    
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" 
+                    alt="${pokemon.name}">
+                </div>
+        </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+pokeAPI.getPokemons().then((pokemons = []) => {
+    listHTML = pokemons.map(convertPokemonToLi).join('')    
+    pokemonList.innerHTML = listHTML
+})
