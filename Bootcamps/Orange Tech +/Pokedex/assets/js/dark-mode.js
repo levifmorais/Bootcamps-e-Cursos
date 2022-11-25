ab2726d3d644074a325c98ff2e493c66017a7baa
levifmorais <102878183+levifmorais@users.noreply.github.com>
2022-11-24 21:43:18 -0300

    dark mode da página e dos elementos

---
+++ Bootcamps/Orange Tech +/Pokedex/assets/js/dark-mode.js
@@ -0,0 +1,46 @@
+
+toggleDarkMode = document.getElementById('toggleDarkMode')
+
+var darkModeOn = localStorage.getItem("isDarkModeOn");
+
+function darkMode() {
+    var body = document.body;
+    var h1 = document.getElementsByTagName("h1");
+    var h3 = document.getElementsByTagName("h3");
+    var content = document.getElementsByClassName("content");
+
+    body.classList.toggle("dark-mode");
+
+    for (let i = 0; i < content.length; i++) {
+        content[i].classList.toggle("dark-mode");
+        h1[i].classList.toggle("dark-mode-text");
+    }
+    
+    for (let i = 0; i < h3.length; i++) {
+        h3[i].classList.toggle("dark-mode-text");
+    }
+
+}
+
+toggleDarkMode.addEventListener('click', () => {
+    darkMode();
+    
+    if(null === darkModeOn){
+        darkModeOn = false;
+    }
+
+    if (darkModeOn == false || darkModeOn == "false") {
+        document.getElementById("switch").innerText = "Light Mode";
+        localStorage.setItem("isDarkModeOn", true);
+        darkModeOn = localStorage.getItem("isDarkModeOn");
+    }
+    else {
+        document.getElementById("switch").innerText = "Dark Mode";
+        localStorage.setItem("isDarkModeOn", false); 
+        darkModeOn = localStorage.getItem("isDarkModeOn");
+    }
+
+    console.log(darkModeOn);
+})
+
+console.log(localStorage.getItem("isDarkModeOn"))
